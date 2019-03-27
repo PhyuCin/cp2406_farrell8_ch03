52ececfb5328ba5bbe438525d32407786bbf18a2
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-27 23:29:22 +0800

    complete Percentages exercise

---
+++ Prac03/Percentages.java
@@ -0,0 +1,16 @@
+public class Percentages {
+
+    public static void main(String[] args){
+        double num1 = 2.5;
+        double num2 = 5.0;
+
+        computePercent(num1, num2);
+        computePercent(num2, num1);
+    }
+
+    public static void computePercent(double num1, double num2){
+        double percent = num1 * 100/num2;
+        System.out.println(num1 + " is " + String.format("%.2f", percent) + " percent of " + num2);
+
+    }
+}
