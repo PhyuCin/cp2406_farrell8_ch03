3ed369f7822f9ef3e80fa1d6116886a0ec00e545
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-27 23:18:53 +0800

    complete NumbersDemo2

--- Prac03/NumbersDemo2.java
+++ Prac03/NumbersDemo2.java
@@ -1,11 +1,13 @@
-public class NumbersDemo {
+import java.util.Scanner;
+
+public class NumbersDemo2 {
 
     public static void displayTwiceTheNumber(int num){
         System.out.println("Twice the number is: " + (num*2));
     }
 
     public static void displayNumberPlusFive(int num) {
-        System.out.println("Number plus five is " + (num + 5));
+        System.out.println("Number plus five is: " + (num + 5));
     }
 
     public static void displayNumberSquared(int num){
@@ -13,19 +15,25 @@ public class NumbersDemo {
     }
 
     public static void main(String[] args){
-        int firstNum = 2;
-        int secondNum = 3;
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("The number is: " + firstNum);
+        System.out.println("Enter first number: ");
+        String stringFirstNum = input.nextLine();
+        int firstNum = Integer.parseInt(stringFirstNum);
+
+        System.out.println("Enter second number: ");
+        String stringSecondNum = input.nextLine();
+        int secondNum = Integer.parseInt(stringSecondNum);
+
+        System.out.println("\nNumber: " + firstNum);
         displayTwiceTheNumber(firstNum);
         displayNumberPlusFive(firstNum);
         displayNumberSquared(firstNum);
 
-        System.out.println("\nThe number is: " + secondNum);
+        System.out.println("\nNumber: " + secondNum);
         displayTwiceTheNumber(secondNum);
         displayNumberPlusFive(secondNum);
         displayNumberSquared(secondNum);
     }
-
 
 }
